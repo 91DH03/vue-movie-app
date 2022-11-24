7db5dcef9157acf7631c996e907d1689421b8a6f
kongdonghwi <withjessi89@gmail.com>
2022-11-24 09:44:20 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'DH',
+    email: 'abcdefghijklmopqrstuvwxyz@gmail.com',
+    blog: 'https://naver.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
